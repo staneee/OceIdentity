754cb3117003fd60f0a27cc89be133c6e04f411c
stan e <yi.hang@live.com>
2018-09-26 18:32:12 +0800

    identityService添加

---
+++ Common/IdentityServerConfig.cs
@@ -0,0 +1,84 @@
+﻿using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+
+namespace Common
+{
+    public class IdentityServerConfig
+    {
+        public const string client_1 = "client";
+        public const string client_2 = "ro.client";
+        public const string MsgApi = "msg_api";
+        public const string PhoneApi = "phone_api";
+        public const string Secret = "MySecret";
+
+
+        // scopes define the API resources in your system
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource(MsgApi, "Msg"),
+                new ApiResource(PhoneApi, "Phone")
+            };
+        }
+
+        // clients want to access resources (aka scopes)
+        public static IEnumerable<Client> GetClients()
+        {
+            // client credentials client
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = client_1,
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret(Secret.Sha256())
+                    },
+                    AllowedScopes =
+                    {
+                       MsgApi,
+                    }
+                },
+                // resource owner password grant client
+                new Client
+                {
+                    ClientId = client_2,
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+
+                    ClientSecrets =
+                    {
+                        new Secret(Secret.Sha256())
+                    },
+                    AllowedScopes =
+                    {
+                       PhoneApi
+                    }
+                }
+            };
+        }
+
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "1",
+                    Username = "admin",
+                    Password = "123"
+                },
+                new TestUser
+                {
+                    SubjectId = "2",
+                    Username = "root",
+                    Password = "321"
+                }
+            };
+        }
+    }
+
+}
---
+++ IDS4Service/Startup.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Common;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace IDS4Service
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+              // configure identity server with in-memory stores, keys, clients and scopes
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential()
+                .AddInMemoryApiResources(IdentityServerConfig.GetApiResources())
+                .AddInMemoryClients(IdentityServerConfig.GetClients())
+                .AddTestUsers(IdentityServerConfig.GetUsers());
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseIdentityServer();
+        }
+    }
+}
