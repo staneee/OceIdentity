6f64b0eb9dec28787e636fb5a3fb09f8fbcf2acf
stan e <yi.hang@live.com>
2018-09-26 18:22:03 +0800

    添加项目文件。

---
+++ OcelotGateway/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace OcelotGateway
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .ConfigureAppConfiguration((hostingContext, builder) =>
+                {
+                    // Ocelot配置文件
+                    builder.AddJsonFile("ocelot.json", false, true);
+                });
+
+        //public static IWebHost CreateWebHostBuilder(string[] args) =>
+        //    WebHost.CreateDefaultBuilder(args)
+        //        .UseStartup<Startup>()
+        //        .ConfigureAppConfiguration((hostingContext, builder) =>
+        //        {
+
+        //        })
+        //        .Build();
+    }
+}
